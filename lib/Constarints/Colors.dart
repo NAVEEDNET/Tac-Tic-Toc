ec6d1aaff6239d2779a400b5aa00bf698effdadd
uoptrainee <93482467+NAVEEDNET@users.noreply.github.com>
2024-01-23 10:00:56 +0530

    changes
    colors and pubspec.yml files

--- lib/Constarints/Colors.dart
+++ lib/Constarints/Colors.dart
@@ -4,4 +4,5 @@ class MainColor{
   static Color primaryColor = Color(0xFFff4b4b);
   static Color secondaryColor = Color(0xFFffca27);
   static Color accentColor = Color(0xFF4169e8);
+  static Color primaryColor2 = Color(0xFF7895e2);
 }
